a56e92f7c11a7d649de178c71d7789e7d503e155
Kristiyan Dimitrov <kdimitrov@billiontoone.com>
2021-02-12 19:08:09 +0000

    completed models section of course; created lifetime_value column in dim_customers and fct_orders.sql table

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,46 +1,29 @@
-WITH 
-
--- CTE 1
-
-customers AS (
-    SELECT *
-    FROM {{ ref('stg_customers') }}
+with customers as (
+    select * from {{ ref('stg_customers')}}
 ),
-
--- CTE 2
-
-orders AS (
-    SELECT *
-    FROM {{ ref('stg_orders') }}
+orders as (
+    select * from {{ ref('fct_orders')}}
 ),
-
--- CTE 3
-
-customer_orders AS (
-    SELECT customer_id
-           , min(order_date) AS first_order_date
-           , max(order_date) AS most_recent_order_date
-           , count(order_id) AS number_of_orders
-    FROM orders
-    GROUP BY 1
+customer_orders as (
+    select
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
+    from orders
+    group by 1
 ),
-
--- CTE 4
-
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
-
 )
-
-SELECT * FROM final
+select * from final
