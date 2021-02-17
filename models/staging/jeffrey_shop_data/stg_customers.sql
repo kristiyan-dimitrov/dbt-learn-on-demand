8b0bf059ad2d1f1249a92ad1442ef876f4399487
Kristiyan Dimitrov <kdimitrov@Kristiyans-MacBook-Pro.local>
2021-02-16 19:46:06 -0600

    Added sources.yml and moved staging_docs.md and schema.yml to a more logical place

--- models/staging/jeffrey_shop_data/stg_customers.sql
+++ models/staging/jeffrey_shop_data/stg_customers.sql
@@ -2,7 +2,7 @@ WITH customers AS (SELECT ID AS customer_id
                           , first_name
                           , last_name
                           
-                    FROM jeffrey_shop_data.customers)
+                    FROM {{ source('jeffrey_shop_data', 'customers') }} )
 
 
 SELECT * FROM customers
